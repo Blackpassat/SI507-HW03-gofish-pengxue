f741aca19c0f0c3b2ff0d4e46bc311755f03b7cb
RaymondXue <raymond@RaymondXues-MacBook-Pro.local>
2018-09-20 14:23:41 -0400

    Added Card, Deck, Hand class from homework2

---
+++ go_fish.py
@@ -0,0 +1,129 @@
+class Card(object):
+	suit_names =  ["Diamonds","Clubs","Hearts","Spades"]
+	rank_levels = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+	faces = {1:"Ace",11:"Jack",12:"Queen",13:"King"}
+
+	def __init__(self, suit=0,rank=2):
+		self.suit = self.suit_names[suit]
+		if rank in self.faces: # self.rank handles printed representation
+			self.rank = self.faces[rank]
+		else:
+			self.rank = rank
+		self.rank_num = rank # To handle winning comparison
+
+	def __str__(self):
+		return "{} of {}".format(self.rank,self.suit)
+
+class Deck(object):
+	def __init__(self): # Don't need any input to create a deck of cards
+		# This working depends on Card class existing above
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1,14):
+				card = Card(suit,rank)
+				self.cards.append(card) # appends in a sorted order
+
+	def __str__(self):
+		total = []
+		for card in self.cards:
+			total.append(card.__str__())
+		# shows up in whatever order the cards are in
+		return "\n".join(total) # returns a multi-line string listing each card
+
+	def pop_card(self, i=-1):
+		# removes and returns a card from the Deck
+		# default is the last card in the Deck
+		return self.cards.pop(i) # this card is no longer in the deck -- taken off
+
+	def shuffle(self):
+		random.shuffle(self.cards)
+
+	def replace_card(self, card):
+		card_strs = [] # forming an empty list
+		for c in self.cards: # each card in self.cards (the initial list)
+			card_strs.append(c.__str__()) # appends the string that represents that card to the empty list
+		if card.__str__() not in card_strs: # if the string representing this card is not in the list already
+			self.cards.append(card) # append it to the list
+
+	def sort_cards(self):
+		# Basically, remake the deck in a sorted way
+		# This is assuming you cannot have more than the normal 52 cars in a deck
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1,14):
+				card = Card(suit,rank)
+				self.cards.append(card)
+
+	def deal(self, hand_num, card_num):
+		cards = []
+		hands = []
+		if (hand_num * card_num > 52) or (card_num == -1):
+			num = 52 // hand_num
+			residual = 52 % hand_num
+			for i in range(hand_num):
+				cards.append(self.cards[i*num:(i+1)*num])
+			for i in range(hand_num):
+				hands.append(Hand(cards[i]))
+			for i in range(residual):
+				hands[i].add_card(self.pop_card())
+		else:
+			for i in range(hand_num):
+				cards.append(self.cards[i*card_num:(i+1)*card_num])
+			for i in range(hand_num):
+				hands.append(Hand(cards[i]))
+		
+		return hands			
+
+class Hand:
+	# create the Hand with an initial set of cards
+	# param: a list of cards
+	def __init__(self, init_cards):
+		self.cards = []
+		for card in init_cards:
+			self.cards.append(card)
+
+	# add a card to the hand
+	# silently fails if the card is already in the hand
+	# param: the card to add
+	# returns: nothing
+	def add_card(self, card):
+		card_strs = []
+		for c in self.cards:
+			card_strs.append(c.__str__())
+		if card.__str__() not in card_strs:
+			self.cards.append(card)
+
+	# remove a card from the hand
+	# param: the card to remove
+	# returns: the card, or None if the card was not in the Hand
+	def remove_card(self, card):
+		card_strs = []
+		for c in self.cards:
+			card_strs.append(c.__str__())
+		if card.__str__() not in card_strs:
+			return None
+		else:
+			return self.cards.pop(self.cards.index(card))
+
+	# draw a card from a deck and add it to the hand
+	# side effect: the deck will be depleted by one card
+	# param: the deck from which to draw
+	# returns: nothing
+	def draw(self, deck):
+		card = deck.pop_card()
+		self.add_card(card)
+
+	def remove_pairs(self):
+		ranks = []
+		for card in self.cards:
+			ranks.append(card.rank_num)
+		new_card_ranks = []
+		for i in ranks:
+			if i not in new_card_ranks:
+				new_card_ranks.append(i)
+		cards = []
+		for i in new_card_ranks:
+			occur = ranks.count(i)
+			if i%2 == 1:
+				cards.append(self.cards[ranks.index(i)])
+		self.cards = cards
